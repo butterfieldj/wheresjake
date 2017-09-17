8a209448ea728e1f00881735a58caf31f951667c
Jacob Butterfield <jbutterfield@qualtrics.com>
2017-09-16 22:08:30 -0600

    Login

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import mongo_data
+#import mongo_data
 from sanic import Sanic
 from sanic.response import json
+from sanic import response
 import os
 
 app = Sanic(__name__)
@@ -29,10 +30,30 @@ async def set_location(request):
     mongo_data.set_location(request.json['latitude'], request.json['longitude'])
     return json({'status':'success'})
 
+@app.route('/login', methods=['GET'])
+async def get_login_page(request):
+    return await response.file('./static/login.html')
+
+@app.route('/login', methods=['POST'])
+async def login(request):
+    print(request.form['name'][0])
+    if 'name' in request.form and request.form['name'][0] == 'Roxana':
+        resp = response.redirect('/home')
+        resp.cookies['tkn'] = '123'
+        return resp
+    else:
+        return await response.file('./static/login.html')
+
+@app.route('/home', methods=['GET'])
+async def home(request):
+    return await response.file('./static/index.html')
+
 @app.middleware('request')
 async def authenticate(request):
-    if request.cookies.get('tkn') == None or request.cookies.get('tkn') != os.environ.get('TOKEN'):
-        return json({'error': 'unauthorized'}, status=401)
+    if ((request.cookies.get('tkn') == None or 
+        request.cookies.get('tkn') != '123') and
+        request.path != '/login'):
+        return response.redirect('/login')
 
 app.static('/', './static')
 
